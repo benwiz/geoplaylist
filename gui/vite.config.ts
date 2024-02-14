3518dcd8d8b18570e6994fcaae27d307fda0d3ce
Kurt Hoefer <kurthoef@gmail.com>
2024-02-14 15:12:57 -0600

    cannot get this ascii to render from the txt

--- gui/vite.config.ts
+++ gui/vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { resolve } from 'pathe';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  publicDir: 'public',
+  resolve: {
+    alias: {
+      '/@': resolve(__dirname, './'),
+    },
+  },
+});
