548f00273188ed1308007cd3056411ff02187239
Kurt Hoefer <kurthoef@gmail.com>
2024-03-08 13:55:45 -0600

    added types, pulled out fetch into utility

--- gui/src/components/AsciiThing/AsciiThing.tsx
+++
@@ -1,7 +0,0 @@
-export const fetchAsciiArt = async () => {
-  try {
-    return (await fetch('./jurassic_park.txt')).text();
-  } catch (error) {
-    console.error('Error fetching ASCII art:', error);
-  }
-};
