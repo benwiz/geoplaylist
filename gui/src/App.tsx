a6106b137d3a4d5c1b9508f97ac2d0592ad45343
Ben Wiz <bwisialowski@gmail.com>
2024-03-15 10:18:06 -0500

    example fetch calls

--- gui/src/App.tsx
+++ gui/src/App.tsx
@@ -6,6 +6,18 @@ const App: React.FC = () => {
   const [isLoading, setIsLoading] = useState<boolean>(true);
   const [asciiArt, setAsciiArt] = useState<string | null>(null);
 
+    useEffect(() => {
+        fetch("http://localhost:8008/test/health")
+            .then((r) => r.json())
+            .then(console.log);
+        fetch("http://localhost:8008/test/csv")
+            .then((r) => r.text())
+            .then(console.log);
+        fetch("http://localhost:8008/pretrained")
+            .then((r) => r.text())
+            .then(console.log);
+    }, [])
+
   useEffect(() => {
     // Simulate a 2-second loading state when the component first mounts
     const initialLoadingTimeout = setTimeout(() => {
