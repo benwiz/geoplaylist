13b6126fb5dc6624ff85b97efe126a098a6a0917
Kurt Hoefer <kurthoef@gmail.com>
2024-08-16 15:07:17 -0500

    touchup

--- gui/src/components/Nav/Nav.tsx
+++ gui/src/components/Nav/Nav.tsx
@@ -1,23 +1,23 @@
-import { styled } from '@stitches/react';
+import { styled } from "@stitches/react";
 // import Logo from './Logo';
-import Logo from './Logo';
+import Logo from "./Logo";
 
-const CanvasContainer = styled('div', {
-  position: 'absolute',
+const CanvasContainer = styled("div", {
+  position: "absolute",
   top: 0,
   left: 0,
-  pointerEvents: 'none',
-  width: '100%',
-  height: '100%',
+  pointerEvents: "none",
+  width: "240px",
+  height: "240px",
 });
 
-const Container = styled('div', {
-  display: 'flex',
-  justifyContent: 'flex-start',
-  height: '60px',
-  marginBottom: '1000px',
+const Container = styled("div", {
+  display: "flex",
+  justifyContent: "flex-start",
+  height: "60px",
+  marginBottom: "620px",
   img: {
-    height: '100%',
+    height: "100%",
   },
 });
 
@@ -30,8 +30,8 @@ function Nav() {
       <Container>
         <a
           // link back to homepage
-          href='./'
-          target='_blank'
+          href="./"
+          target="_blank"
         ></a>
         <div>
           <p>oh wow, what a nav cmpt</p>
